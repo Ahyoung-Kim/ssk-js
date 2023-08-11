2895c5d0ebe10c9ab1f4da611602dad67d421f5e
Ahyoung-Kim <eileel@naver.com>
2023-08-11 16:46:21 +0900

    :art: 일정 디테일 바텀시트 & 일정 디테일 수정 바텀시트 분리
    일정 디테일 바텀시트 & 일정 디테일 수정 바텀시트 분리

--- src/components/calendar/ScheduleDetailBSheet.jsx
+++ src/components/calendar/ScheduleDetailBSheet.jsx
@@ -1,24 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
 
 import styled from "styled-components/native";
-import color from "../../common/color";
-import tags from "../../common/tags";
 
-import ConfirmBtnBottomSheet from "../common/ConfirmBtnBottomSheet";
+import BottomSheet from "../common/BottomSheet";
 import CalendarBSheetHeader from "./CalendarBSheetHeader";
 import LeftBarContainer from "../common/LeftBarContainer";
 import TimePicker from "../common/TimePicker";
 import SelectTag from "../inputs/SelectTag";
 import UserInfo from "../common/UserInfo";
-import {
-  dateToTimeFormat,
-  serverDateFormat,
-  timeFormatToDate,
-} from "../../utils/date";
-
-import { Alert } from "react-native";
-import client from "../../config/axios";
-import DatePickerForm from "../inputs/DatePickerForm";
+import { timeFormatToDate } from "../../utils/date";
+import UpdateScheduleBSheet from "./UpdateScheduleBSheet";
 
 const ScheduleDetailBSheet = ({ rbRef, schedule, date, edit, setRefetch }) => {
   // console.log("schedule: ", schedule);
@@ -32,103 +23,26 @@ const ScheduleDetailBSheet = ({ rbRef, schedule, date, edit, setRefetch }) => {
     tutoringId,
   } = schedule;
 
-  const today = new Date();
-  today.setMinutes(0);
-  const [startTimeWant, setStartTimeWant] = useState(today);
-  const [endTimeWant, setEndTimeWant] = useState(today);
-  const [dateWant, setDateWant] = useState(today);
+  const updateScheduleRbRef = useRef();
 
-  const [tag, setTag] = useState(0);
-  const [description, setDescription] = useState("");
-
-  const handleUpdateSchedule = async () => {
-    const body = {
-      tutoringId,
-      date: serverDateFormat(date),
-      startTime,
-      endTime,
-      dateWant: serverDateFormat(dateWant),
-      startTimeWant: dateToTimeFormat(startTimeWant),
-      endTimeWant: dateToTimeFormat(endTimeWant),
-    };
-    try {
-      const ret = await client.put("/api/schedule", body);
-
-      if (ret.status == 200) {
-        Alert.alert("일정 편집", "일정 정보가 편집되었습니다.");
-        setRefetch(true);
-        rbRef?.current?.close();
-      }
-    } catch (err) {
-      console.log("update schedule error: ", err);
-      const status = err?.response?.status;
-
-      if (status == 409) {
-        // Conflict
-        console.log("create schedule: conflict");
-        Alert.alert("일정 편집 실패", "해당 시간에 중복되는 수업이 있습니다.");
-      }
-    }
+  const handlePressEdit = () => {
+    updateScheduleRbRef?.current?.open();
   };
-
-  const deleteSchedule = async () => {
-    const body = {
-      tutoringId,
-      date: serverDateFormat(date),
-      startTime,
-      endTime,
-    };
-
-    console.log(body);
-
-    try {
-      const ret = await client.delete("/api/schedule", {
-        data: body,
-      });
-
-      if (ret.status == 200) {
-        setRefetch(true);
-        rbRef?.current?.close();
-      }
-    } catch (err) {
-      console.log("delete schedule err: ", err);
-    }
-  };
-
-  const handleDeleteSchedule = () => {
-    Alert.alert("삭제", "해당 일정을 삭제하시겠습니까?", [
-      {
-        text: "취소",
-        onPress: () => {},
-        style: "cancel",
-      },
-      {
-        text: "삭제",
-        onPress: deleteSchedule,
-      },
-    ]);
-  };
-
-  useEffect(() => {
-    setTag(tagColor);
-    setStartTimeWant(timeFormatToDate(startTime));
-    setEndTimeWant(timeFormatToDate(endTime));
-    setDateWant(date);
-  }, [schedule]);
 
   return (
     <>
-      <ConfirmBtnBottomSheet
+      <BottomSheet
         rbRef={rbRef}
-        heightPercentage={0.9}
-        cancelText="삭제"
-        confirmText="편집"
-        filled={true}
-        buttonColor={color.COLOR_MAIN}
-        onCancel={handleDeleteSchedule}
-        onConfirm={handleUpdateSchedule}
+        heightPercentage={0.6}
+        button="진도 노트"
+        handlePressButton={() => {}}
+        onClose={() => {}}
       >
-        <CalendarBSheetHeader date={date} edit={edit} />
+        <CalendarBSheetHeader
+          date={date}
+          edit={edit}
+          handlePressEdit={handlePressEdit}
+        />
 
         <LeftBarContainer label="Info">
           <UserInfo
@@ -138,55 +52,36 @@ const ScheduleDetailBSheet = ({ rbRef, schedule, date, edit, setRefetch }) => {
           />
         </LeftBarContainer>
 
-        <DatePickerForm
-          label="Date"
-          date={dateWant}
-          setDate={setDateWant}
-          leftBar={true}
-          edit={edit}
-        />
-
         <TimePicker
-          startTime={startTimeWant}
-          setStartTime={setStartTimeWant}
-          endTime={endTimeWant}
-          setEndTime={setEndTimeWant}
-          clickable={edit}
+          startTime={timeFormatToDate(startTime)}
+          endTime={timeFormatToDate(endTime)}
+          clickable={false}
         />
 
         <LeftBarContainer label="Description">
-          {edit ? (
-            <DescriptionInput
-              multiline={true} //여러줄 입력가능
-              textAlignVertical="top" //처음부터 시작 (기본값은 center)
-              placeholder="내용을 입력하세요."
-              value={description}
-              onChangeText={setDescription}
-            />
-          ) : (
-            <DescriptionText>
-              디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션
-            </DescriptionText>
-          )}
+          <DescriptionText>
+            디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션디스크립션
+          </DescriptionText>
         </LeftBarContainer>
 
-        <SelectTag tag={tag} setTag={setTag} edit={false} />
-      </ConfirmBtnBottomSheet>
+        <SelectTag tag={tagColor} edit={false} />
+
+        {/* 일정 수정 바텀시트 */}
+        {edit && (
+          <UpdateScheduleBSheet
+            rbRef={updateScheduleRbRef}
+            scheduleRbRef={rbRef}
+            schedule={schedule}
+            date={date}
+            setRefetch={setRefetch}
+          />
+        )}
+      </BottomSheet>
     </>
   );
 };
 
 export default ScheduleDetailBSheet;
-
-const DescriptionInput = styled.TextInput`
-  background-color: ${color.COLOR_WHITE_BACKGROUND};
-  border-width: 1;
-  border-color: ${color.COLOR_GRAY_BORDER};
-  width: 100%;
-  height: 150;
-  border-radius: 5;
-  padding: 7px;
-`;
 
 const DescriptionText = styled.Text`
   // font-weight: bold;
