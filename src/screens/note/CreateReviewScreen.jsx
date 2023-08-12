6c715d081197477d28bd072f0721af990cd01c18
Ahyoung-Kim <eileel@naver.com>
2023-08-12 19:13:57 +0900

    :art: 수업 일지 생성: 복습 노트 추가 부분 수정
    수업 일지 생성: 복습 노트 추가 부분 수정

--- src/screens/note/CreateReviewScreen.jsx
+++ src/screens/note/CreateReviewScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import styled from "styled-components/native";
 import color from "../../common/color";
@@ -14,7 +14,32 @@ import DropDownForm from "../../components/inputs/DropDownForm";
 import KeyboardAvoidingLayout from "../../components/common/KeyboardAvoidingLayout";
 import BigButton from "../../components/common/BigButton";
 import client from "../../config/axios";
-import { Alert } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
+import { Feather, FontAwesome5 } from "@expo/vector-icons";
+
+const EachReviewItem = ({ review, onPressXButton }) => {
+  const { body, tagId, tag } = review;
+
+  return (
+    <>
+      <ReviewContainer>
+        <ReviewWrapper>
+          <FontAwesome5 name="tag" size={14} color={color.COLOR_GRAY_ICON} />
+
+          <TagContainer>
+            <TagName>{tag.name}</TagName>
+          </TagContainer>
+
+          <ReviewName>{body}</ReviewName>
+        </ReviewWrapper>
+
+        <TouchableOpacity onPress={onPressXButton}>
+          <Feather name="x" size={18} color={color.COLOR_GRAY_ICON} />
+        </TouchableOpacity>
+      </ReviewContainer>
+    </>
+  );
+};
 
 const CreateReviewScreen = () => {
   const navigation = useNavigation();
@@ -25,17 +50,49 @@ const CreateReviewScreen = () => {
   const [reviewList, setReviewList] = useState([]);
   // 복습 내용
   const [body, setBody] = useState("");
-  // 태그 아이디
-  const [tagId, setTagId] = useState(null);
+  // 태그
+  const [tag, setTag] = useState(null);
+
+  const [tagList, setTagList] = useState([]);
+
+  const onPressPlustButton = () => {
+    if (!body) {
+      Alert.alert("복습 내용을 입력해주세요!");
+      return;
+    }
+    if (!tag) {
+      Alert.alert("복습 태그를 선택해주세요!");
+      return;
+    }
+
+    setReviewList([
+      ...reviewList,
+      {
+        body,
+        tagId: tag.id,
+        tag,
+      },
+    ]);
+
+    setBody("");
+  };
+
+  const onPressXButton = (idx) => {
+    setReviewList(reviewList.filter((_, index) => index !== idx));
+  };
 
   const handleCreateClassNote = async () => {
+    const parsedReviewList = reviewList.map((review) => ({
+      body: review.body,
+      tagId: review.tagId,
+    }));
     const body = {
       ...prevStates,
-      reviewList,
+      reviewList: parsedReviewList,
       tutoringId,
     };
 
-    // console.log(body);
+    // console.log("create body: ", body);
     try {
       const ret = await client.post(`/api/note`, body);
 
@@ -51,6 +108,30 @@ const CreateReviewScreen = () => {
       console.log("create class note error: ", err);
     }
   };
+
+  const getTagList = async () => {
+    try {
+      const ret = await client.post("/api/tag/list", {
+        tutoringId,
+      });
+
+      if (ret.status == 200) {
+        // console.log(ret.data);
+        setTagList(ret.data.tagList);
+      }
+    } catch (err) {
+      console.log("get tag list error: ", err);
+      const status = err?.response?.status;
+
+      if (status == 404) {
+        setTagList([]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getTagList();
+  }, []);
 
   return (
     <KeyboardAvoidingLayout>
@@ -74,14 +155,38 @@ const CreateReviewScreen = () => {
         <DropDownForm
           label="복습 태그"
           placeholder={"복습 태그를 선택하세요."}
-          list={[{ tagId: 1, tagName: "확통" }]}
-          textKey={"tagName"}
+          list={tagList}
+          textKey={"name"}
           onPressItem={(item) => {
-            console.log(item);
-            setTagId(item.tagId);
+            // console.log(item);
+            setTag(item);
           }}
           menuHeight={150}
         />
+
+        {prevStates && (
+          <Container>
+            <PlusButton onPress={onPressPlustButton}>
+              <FontAwesome5
+                name="plus"
+                size={16}
+                color={color.COLOR_GRAY_ICON}
+              />
+            </PlusButton>
+
+            {reviewList.length > 0 && (
+              <ReviewList>
+                {reviewList.map((review, idx) => (
+                  <EachReviewItem
+                    key={`review_${review.body}`}
+                    review={review}
+                    onPressXButton={onPressXButton.bind(this, idx)}
+                  />
+                ))}
+              </ReviewList>
+            )}
+          </Container>
+        )}
       </MainLayout>
 
       {prevStates ? (
@@ -94,3 +199,48 @@ const CreateReviewScreen = () => {
 };
 
 export default CreateReviewScreen;
+
+const Container = styled.View`
+  padding-horizontal: 15;
+`;
+
+const PlusButton = styled.TouchableOpacity`
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+  border-width: 2;
+  border-color: ${color.COLOR_GRAY_ICON};
+  border-radius: 100;
+  width: 30;
+  height: 30;
+  margin-vertical: 20;
+`;
+
+const ReviewList = styled.View``;
+
+const ReviewContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10;
+`;
+
+const ReviewWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const TagContainer = styled.View`
+  margin-horizontal: 7;
+  padding-horizontal: 8;
+  padding-vertical: 4;
+  border-radius: 5;
+  background-color: rgba(12, 155, 251, 0.2);
+`;
+
+const TagName = styled.Text``;
+
+const ReviewName = styled.Text`
+  font-weight: bold;
+  font-size: 16;
+`;
